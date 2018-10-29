fc6ce7f57730efbe62e9cd138907a35ef4dc266b
Ryan Diel <rdiel01@gmail.com>
2018-10-29 16:31:59 -0500

    add encode

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from html.parser import HTMLParser
-from urllib.request import urlopen
+from urllib.request import urlopen, urlencode
 import os, ssl
 
 class MyHTMLParser(HTMLParser):
@@ -28,7 +28,14 @@ if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     getattr(ssl, '_create_unverified_context', None)): 
     ssl._create_default_https_context = ssl._create_unverified_context
 
-response = urlopen('https://foundrycommerce.helpdocs.com')
+url = '*****'
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+values = {'password':'******'}
+headers = {'User-Agent' : user_agent}
+
+data = urllib.urlencode(values)
+req = urllib2.Request(url, data, headers)
+response = urlopen(req)
 
 copy = ''
 
