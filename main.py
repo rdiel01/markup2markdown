41089d0f0167afab3a8cd7f931f70cfe65350ac2
Ryan Diel <rdiel01@gmail.com>
2018-10-29 14:46:29 -0500

    add comments

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from urllib.request import urlopen
 import os, ssl
 
 class MyHTMLParser(HTMLParser):
+    """
+    look for tags
+    """
     def handle_starttag(self, tag, attrs):
         print("Encountered a start tag:", tag)
 
@@ -18,7 +21,9 @@ def clean(string):
     """
     return string[2:-2]
 
-
+"""
+ssl certification
+"""
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     getattr(ssl, '_create_unverified_context', None)): 
     ssl._create_default_https_context = ssl._create_unverified_context
@@ -27,7 +32,7 @@ response = urlopen('https://foundrycommerce.helpdocs.com')
 
 copy = ''
 
-for i in response:
+for i in response: # step through bytes in object, clean out /b and /n, concat to blank string
     copy += clean(str(i))
 
 print(copy)
